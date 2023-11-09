6fba37dbaa23573f3819dc53e8074d7434d71b2c
dharmesh-kota <dharmeshkota123@gmail.com>
2023-11-09 21:22:34 +0530

    Pharmacy Functionalities done!

--- controller/patient_controller.js
+++ controller/patient_controller.js
@@ -24,3 +24,24 @@ module.exports.make_appointment = async (req, res) => {
         return res.status(500).json({error: 'Server Error!'});
     }
 }
+
+module.exports.track_appointment = async (req, res) => {
+    try {
+        
+        // given the patient's email in req.user.email, find out the appointments booked by the patients
+        // All the appointments, i.e., pending bit 0 as well as 1
+        // write two diiferent different queries for upcoming and past appointments
+        let upcoming_appointments = await pool.query(``);
+        let past_appointments = await pool.query(``);
+
+        return res.render('patient-appointments', {
+            title: 'Appointments',
+            upcoming_appointments: upcoming_appointments,
+            past_appointments: past_appointments
+        });
+        
+    } catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(500).json({error: 'Server Error!'});
+    }
+}
