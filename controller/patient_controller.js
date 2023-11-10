fe0fb14b2dcf5a865c9ab3bd24a0e6e25456937f
krushisutariya <krushisutr@gmail.com>
2023-11-10 10:51:11 +0530

    track_appointment

--- controller/patient_controller.js
+++ controller/patient_controller.js
@@ -27,17 +27,15 @@ module.exports.make_appointment = async (req, res) => {
 
 module.exports.track_appointment = async (req, res) => {
     try {
-        
         // given the patient's email in req.user.email, find out the appointments booked by the patients
         // All the appointments, i.e., pending bit 0 as well as 1
         // write two diiferent different queries for upcoming and past appointments
-        //
+        
         let upcoming_appointments = await pool.query(`select * from appoints where patient_email=$1 && is_pending =0`,[req.user.email]);
         upcoming_appointments = upcoming_appointments.rows;
 
         let past_appointments = await pool.query(`select * from appoints where patient_email=$1 && is_pending =1`,[req.user.email]);
         past_appointments = past_appointments.rows;
-       
         return res.render('patient-appointments', {
             title: 'Appointments',
             upcoming_appointments: upcoming_appointments,
